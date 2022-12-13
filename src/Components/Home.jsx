dc0cd870ae09e31015ca8c5e19aa81f6f4f2cc4a
Siddharthan Dileep <siddharthandileep@gmail.com>
2022-12-13 20:56:11 +0530

    Component update UI update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
-        <p className='text-pink-600'>Hi, my name is</p>
+        <p className='text-pink-600 text-xl'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-5xl md:6xl lg:7xl font-bold text-[#ccd6f6]'>
           SIDDHARTHAN DILEEP
         </h1>
